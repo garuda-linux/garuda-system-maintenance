9336f25b7a43c919b37ee40fb34030e295be9525
TNE <totallynotelite@gmail.com>
2021-07-03 23:51:05 +0200

    Set application display name

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     a.setApplicationName("garuda-system-maintenance");
     a.setOrganizationName("garuda");
+    a.setApplicationDisplayName("Garuda System Maintenance");
     Tray t;
     return a.exec();
 }
