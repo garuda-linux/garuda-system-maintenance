9d9e52fdda22e1b5060351e69991b93b54668756
Garuda Linux CI/CD <gitlab@garudalinux.org>
2025-09-10 23:06:06 +0000

    chore(i18n): Pull Transifex translations

--- i18n/garuda-system-maintenance_de.ts
+++ i18n/garuda-system-maintenance_de.ts
@@ -1,17 +1,15 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="de_DE" sourcelanguage="en_US">
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="de" sourcelanguage="en">
 <context>
     <name>ForumAgent</name>
     <message>
         <location filename="../src/forumagent.cpp" line="63"/>
         <source>New maintenance announcement: </source>
-        <translation type="unfinished"></translation>
+        <translation>Neue Wartungsankündigung:</translation>
     </message>
     <message>
         <location filename="../src/forumagent.cpp" line="65"/>
         <source>Open in browser</source>
-        <translation type="unfinished"></translation>
+        <translation>Im Browser öffnen</translation>
     </message>
 </context>
 <context>
@@ -19,45 +17,46 @@
     <message>
         <location filename="../src/packageagent.cpp" line="75"/>
         <source>Hotfix available!</source>
-        <translation type="unfinished"></translation>
+        <translation>Hotfix verfügbar!</translation>
     </message>
     <message>
         <location filename="../src/packageagent.cpp" line="75"/>
         <source>Important hotfix update found. Do you want to apply the hotfix (recommended)?
 Press the help button to learn more about this hotfix.</source>
-        <translation type="unfinished"></translation>
+        <translation>Wichtiges Hotfix-Update gefunden. Möchten Sie das Hotfix anwenden (empfohlen)?
+Klicken Sie auf die Hilfe-Schaltfläche, um mehr über dieses Hotfix zu erfahren.</translation>
     </message>
     <message>
         <location filename="../src/packageagent.cpp" line="88"/>
         <location filename="../src/packageagent.cpp" line="109"/>
         <location filename="../src/packageagent.cpp" line="111"/>
         <source>Garuda System Maintenance</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../src/packageagent.cpp" line="88"/>
         <source>Updating keyrings in the background...</source>
-        <translation type="unfinished"></translation>
+        <translation>Keyrings werden im Hintergrund aktualisiert …</translation>
     </message>
     <message>
         <location filename="../src/packageagent.cpp" line="109"/>
         <source>Hotfix successfully applied!</source>
-        <translation type="unfinished"></translation>
+        <translation>Hotfix erfolgreich angewendet!</translation>
     </message>
     <message>
         <location filename="../src/packageagent.cpp" line="109"/>
         <source>Keyrings successfully updated!</source>
-        <translation type="unfinished"></translation>
+        <translation>Keyring erfolgreich aktualisiert!</translation>
     </message>
     <message>
         <location filename="../src/packageagent.cpp" line="111"/>
         <source>Hotfix failed!</source>
-        <translation type="unfinished"></translation>
+        <translation>Hotfix fehlgeschlagen!</translation>
     </message>
     <message>
         <location filename="../src/packageagent.cpp" line="111"/>
         <source>Keyring update failed!</source>
-        <translation type="unfinished"></translation>
+        <translation>Keyring-Update fehlgeschlagen!</translation>
     </message>
 </context>
 <context>
@@ -70,37 +69,37 @@ Press the help button to learn more about this hotfix.</source>
     <message>
         <location filename="../src/settingsdialog.ui" line="27"/>
         <source>Automatically update core keyrings</source>
-        <translation type="unfinished"></translation>
+        <translation>Keyrings automatisch aktualisieren</translation>
     </message>
     <message>
         <location filename="../src/settingsdialog.ui" line="52"/>
         <source>Prompt to apply critical hotfixes immediately</source>
-        <translation type="unfinished"></translation>
+        <translation>Zur sofortigen Anwendung kritischer Hotfixes auffordern</translation>
     </message>
     <message>
         <location filename="../src/settingsdialog.ui" line="64"/>
         <source>Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Benachrichtigungen</translation>
     </message>
     <message>
         <location filename="../src/settingsdialog.ui" line="70"/>
         <source>Notify about forum maintenance announcement posts</source>
-        <translation type="unfinished"></translation>
+        <translation>Benachrichtigen für Systemwartungshinweise vom Forum erhalten</translation>
     </message>
     <message>
         <location filename="../src/settingsdialog.ui" line="77"/>
         <source>Notify about partial upgrades</source>
-        <translation type="unfinished"></translation>
+        <translation>Benachrichtigen Sie über Teil-Upgrades</translation>
     </message>
     <message>
         <location filename="../src/settingsdialog.ui" line="84"/>
         <source>Notify if system is critically out of date</source>
-        <translation type="unfinished"></translation>
+        <translation>Benachrichtigen, wenn das System kritisch veraltet ist</translation>
     </message>
     <message>
         <location filename="../src/settingsdialog.ui" line="91"/>
         <source>Notify about old snapshots</source>
-        <translation type="unfinished"></translation>
+        <translation>Benachrichtigen über alte Snapshots</translation>
     </message>
 </context>
 <context>
@@ -108,44 +107,45 @@ Press the help button to learn more about this hotfix.</source>
     <message>
         <location filename="../src/snapshotagent.cpp" line="24"/>
         <source>Old snapshots/backups found</source>
-        <translation type="unfinished"></translation>
+        <translation>Alte Snapshots/Backups gefunden</translation>
     </message>
     <message>
         <location filename="../src/snapshotagent.cpp" line="25"/>
         <source>Old snapshots or snapshot restore backups have been found that are using up disk space.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>Es wurden alte Snapshots oder Snapshot-Wiederherstellungssicherungen gefunden, die Speicherplatz belegen.
+</translation>
     </message>
     <message>
         <location filename="../src/snapshotagent.cpp" line="26"/>
         <source>View and delete</source>
-        <translation type="unfinished"></translation>
+        <translation>Anzeigen und Löschen</translation>
     </message>
     <message>
         <location filename="../src/snapshotagent.cpp" line="27"/>
         <source>Disable notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Benachrichtigungen deaktivieren</translation>
     </message>
     <message>
         <location filename="../src/snapshotagent.cpp" line="32"/>
         <source>Old snapshots</source>
-        <translation type="unfinished"></translation>
+        <translation>Alte Snapshots</translation>
     </message>
     <message>
         <location filename="../src/snapshotagent.cpp" line="36"/>
         <location filename="../src/snapshotagent.cpp" line="70"/>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>Löschen</translation>
     </message>
     <message>
         <location filename="../src/snapshotagent.cpp" line="66"/>
         <source>Old snapshots/backups found!</source>
-        <translation type="unfinished"></translation>
+        <translation>Alte Snapshots/Backups gefunden!</translation>
     </message>
     <message>
         <location filename="../src/snapshotagent.cpp" line="72"/>
         <source>Ignore permanently</source>
-        <translation type="unfinished"></translation>
+        <translation>Dauerhaft ignorieren</translation>
     </message>
 </context>
 <context>
@@ -153,7 +153,7 @@ Press the help button to learn more about this hotfix.</source>
     <message>
         <location filename="../src/tray.ui" line="14"/>
         <source>Tray</source>
-        <translation type="unfinished"></translation>
+        <translation>Benachrichtigungsbereich</translation>
     </message>
 </context>
 <context>
@@ -162,49 +162,51 @@ Press the help button to learn more about this hotfix.</source>
         <location filename="../src/updateagent.cpp" line="25"/>
         <location filename="../src/updateagent.cpp" line="92"/>
         <source>System out of date</source>
-        <translation type="unfinished"></translation>
+        <translation>System veraltet</translation>
     </message>
     <message>
         <location filename="../src/updateagent.cpp" line="26"/>
         <location filename="../src/updateagent.cpp" line="93"/>
         <source>This system has not been updated in a long time.
 Regularly applying system updates on a rolling release distribution is highly encouraged to avoid various issues.</source>
-        <translation type="unfinished"></translation>
+        <translation>Dieses System wurde schon lange nicht mehr aktualisiert.
+Um verschiedene Probleme zu vermeiden, wird dringend empfohlen, regelmäßig Systemupdates auf einer Rolling-Release-Distribution anzuwenden.</translation>
     </message>
     <message>
         <location filename="../src/updateagent.cpp" line="27"/>
         <location filename="../src/updateagent.cpp" line="67"/>
         <location filename="../src/updateagent.cpp" line="98"/>
         <source>Update system</source>
-        <translation type="unfinished"></translation>
+        <translation>System aktualisieren</translation>
     </message>
     <message>
         <location filename="../src/updateagent.cpp" line="28"/>
         <location filename="../src/updateagent.cpp" line="69"/>
         <source>Disable warnings</source>
-        <translation type="unfinished"></translation>
+        <translation>Warnungen deaktivieren</translation>
     </message>
     <message>
         <location filename="../src/updateagent.cpp" line="68"/>
         <location filename="../src/updateagent.cpp" line="103"/>
         <source>Learn more</source>
-        <translation type="unfinished"></translation>
+        <translation>Mehr erfahren</translation>
     </message>
     <message>
         <location filename="../src/updateagent.cpp" line="92"/>
         <source>Partial upgrade detected</source>
-        <translation type="unfinished"></translation>
+        <translation>Teilweises Upgrade erkannt</translation>
     </message>
     <message>
         <location filename="../src/updateagent.cpp" line="93"/>
         <source>You performed a &quot;partial upgrade&quot;. Please fully update your system to prevent system instability.
 Performing partial ugprades is unsupported.</source>
-        <translation type="unfinished"></translation>
+        <translation>Sie haben ein „partial upgrade“ durchgeführt. Bitte aktualisieren Sie Ihr System vollständig, um Systeminstabilitäten zu vermeiden.
+Partial upgrades werden nicht unterstützt.</translation>
     </message>
     <message>
         <location filename="../src/updateagent.cpp" line="107"/>
         <source>Ignore permanently</source>
-        <translation type="unfinished"></translation>
+        <translation>Dauerhaft ignorieren</translation>
     </message>
 </context>
 </TS>
