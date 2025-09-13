3b8858fe56c0ecbe30b48dccf8ea079f00b2fe2c
Garuda Linux CI/CD <gitlab@garudalinux.org>
2025-09-13 08:10:35 +0000

    chore(i18n): Pull Transifex translations

--- i18n/garuda-system-maintenance_sv.ts
+++ i18n/garuda-system-maintenance_sv.ts
@@ -1,17 +1,15 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="sv" sourcelanguage="en">
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="sv" sourcelanguage="en">
 <context>
     <name>ForumAgent</name>
     <message>
         <location filename="../src/forumagent.cpp" line="63"/>
         <source>New maintenance announcement: </source>
-        <translation type="unfinished"></translation>
+        <translation>Nytt underhållsmeddelande:</translation>
     </message>
     <message>
         <location filename="../src/forumagent.cpp" line="65"/>
         <source>Open in browser</source>
-        <translation type="unfinished"></translation>
+        <translation>Öppna i webbläsare</translation>
     </message>
 </context>
 <context>
@@ -19,45 +17,46 @@
     <message>
         <location filename="../src/packageagent.cpp" line="75"/>
         <source>Hotfix available!</source>
-        <translation type="unfinished"></translation>
+        <translation>Snabbkorrigering tillgänglig!</translation>
     </message>
     <message>
         <location filename="../src/packageagent.cpp" line="75"/>
         <source>Important hotfix update found. Do you want to apply the hotfix (recommended)?
 Press the help button to learn more about this hotfix.</source>
-        <translation type="unfinished"></translation>
+        <translation>Viktig snabbkorrigeringsuppdatering hittades. Vill du installera snabbkorrigeringen (rekommenderas)?
+Tryck på hjälpknappen för att läsa mer om den här snabbkorrigeringen.</translation>
     </message>
     <message>
         <location filename="../src/packageagent.cpp" line="88"/>
         <location filename="../src/packageagent.cpp" line="109"/>
         <location filename="../src/packageagent.cpp" line="111"/>
         <source>Garuda System Maintenance</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../src/packageagent.cpp" line="88"/>
         <source>Updating keyrings in the background...</source>
-        <translation type="unfinished"></translation>
+        <translation>Uppdaterar nyckelringar i bakgrunden...</translation>
     </message>
     <message>
         <location filename="../src/packageagent.cpp" line="109"/>
         <source>Hotfix successfully applied!</source>
-        <translation type="unfinished"></translation>
+        <translation>Snabbkorrigeringen har installerats!</translation>
     </message>
     <message>
         <location filename="../src/packageagent.cpp" line="109"/>
         <source>Keyrings successfully updated!</source>
-        <translation type="unfinished"></translation>
+        <translation>Nyckelringar har uppdaterats!</translation>
     </message>
     <message>
         <location filename="../src/packageagent.cpp" line="111"/>
         <source>Hotfix failed!</source>
-        <translation type="unfinished"></translation>
+        <translation>Snabbkorrigeringen misslyckades!</translation>
     </message>
     <message>
         <location filename="../src/packageagent.cpp" line="111"/>
         <source>Keyring update failed!</source>
-        <translation type="unfinished"></translation>
+        <translation>Nyckelringsuppdateringen misslyckades!</translation>
     </message>
 </context>
 <context>
@@ -65,42 +64,42 @@ Press the help button to learn more about this hotfix.</source>
     <message>
         <location filename="../src/settingsdialog.ui" line="21"/>
         <source>Automatic updates</source>
-        <translation type="unfinished"></translation>
+        <translation>Automatiska uppdateringar</translation>
     </message>
     <message>
         <location filename="../src/settingsdialog.ui" line="27"/>
         <source>Automatically update core keyrings</source>
-        <translation type="unfinished"></translation>
+        <translation>Uppdatera automatiskt kärnnyckelringar</translation>
     </message>
     <message>
         <location filename="../src/settingsdialog.ui" line="52"/>
         <source>Prompt to apply critical hotfixes immediately</source>
-        <translation type="unfinished"></translation>
+        <translation>Uppmana att omedelbart installera kritiska snabbkorrigeringar</translation>
     </message>
     <message>
         <location filename="../src/settingsdialog.ui" line="64"/>
         <source>Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Aviseringar</translation>
     </message>
     <message>
         <location filename="../src/settingsdialog.ui" line="70"/>
         <source>Notify about forum maintenance announcement posts</source>
-        <translation type="unfinished"></translation>
+        <translation>Meddela om inlägg om forumunderhåll</translation>
     </message>
     <message>
         <location filename="../src/settingsdialog.ui" line="77"/>
         <source>Notify about partial upgrades</source>
-        <translation type="unfinished"></translation>
+        <translation>Meddela om deluppgraderingar</translation>
     </message>
     <message>
         <location filename="../src/settingsdialog.ui" line="84"/>
         <source>Notify if system is critically out of date</source>
-        <translation type="unfinished"></translation>
+        <translation>Meddela om systemet är kritiskt föråldrat</translation>
     </message>
     <message>
         <location filename="../src/settingsdialog.ui" line="91"/>
         <source>Notify about old snapshots</source>
-        <translation type="unfinished"></translation>
+        <translation>Meddela om gamla snapshots</translation>
     </message>
 </context>
 <context>
@@ -108,44 +107,45 @@ Press the help button to learn more about this hotfix.</source>
     <message>
         <location filename="../src/snapshotagent.cpp" line="24"/>
         <source>Old snapshots/backups found</source>
-        <translation type="unfinished"></translation>
+        <translation>Gamla snapshots/säkerhetskopior hittades</translation>
     </message>
     <message>
         <location filename="../src/snapshotagent.cpp" line="25"/>
         <source>Old snapshots or snapshot restore backups have been found that are using up disk space.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>Gamla snapshots eller säkerhetskopior av snapshots som återställs har hittats och som använder upp diskutrymme.
+</translation>
     </message>
     <message>
         <location filename="../src/snapshotagent.cpp" line="26"/>
         <source>View and delete</source>
-        <translation type="unfinished"></translation>
+        <translation>Visa och radera</translation>
     </message>
     <message>
         <location filename="../src/snapshotagent.cpp" line="27"/>
         <source>Disable notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Inaktivera aviseringar</translation>
     </message>
     <message>
         <location filename="../src/snapshotagent.cpp" line="32"/>
         <source>Old snapshots</source>
-        <translation type="unfinished"></translation>
+        <translation>Gamla snapshots</translation>
     </message>
     <message>
         <location filename="../src/snapshotagent.cpp" line="36"/>
         <location filename="../src/snapshotagent.cpp" line="70"/>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>Radera</translation>
     </message>
     <message>
         <location filename="../src/snapshotagent.cpp" line="66"/>
         <source>Old snapshots/backups found!</source>
-        <translation type="unfinished"></translation>
+        <translation>Gamla snapshots/säkerhetskopior hittade!</translation>
     </message>
     <message>
         <location filename="../src/snapshotagent.cpp" line="72"/>
         <source>Ignore permanently</source>
-        <translation type="unfinished"></translation>
+        <translation>Ignorera permanent</translation>
     </message>
 </context>
 <context>
@@ -153,7 +153,7 @@ Press the help button to learn more about this hotfix.</source>
     <message>
         <location filename="../src/tray.ui" line="14"/>
         <source>Tray</source>
-        <translation type="unfinished"></translation>
+        <translation>Bricka</translation>
     </message>
 </context>
 <context>
@@ -162,49 +162,51 @@ Press the help button to learn more about this hotfix.</source>
         <location filename="../src/updateagent.cpp" line="25"/>
         <location filename="../src/updateagent.cpp" line="92"/>
         <source>System out of date</source>
-        <translation type="unfinished"></translation>
+        <translation>Systemet är föråldrat</translation>
     </message>
     <message>
         <location filename="../src/updateagent.cpp" line="26"/>
         <location filename="../src/updateagent.cpp" line="93"/>
         <source>This system has not been updated in a long time.
 Regularly applying system updates on a rolling release distribution is highly encouraged to avoid various issues.</source>
-        <translation type="unfinished"></translation>
+        <translation>Det här systemet har inte uppdaterats på länge.
+Att regelbundet installera systemuppdateringar på en rullande utgåva rekommenderas starkt för att undvika diverse problem.</translation>
     </message>
     <message>
         <location filename="../src/updateagent.cpp" line="27"/>
         <location filename="../src/updateagent.cpp" line="67"/>
         <location filename="../src/updateagent.cpp" line="98"/>
         <source>Update system</source>
-        <translation type="unfinished"></translation>
+        <translation>Uppdatera systemet</translation>
     </message>
     <message>
         <location filename="../src/updateagent.cpp" line="28"/>
         <location filename="../src/updateagent.cpp" line="69"/>
         <source>Disable warnings</source>
-        <translation type="unfinished"></translation>
+        <translation>Inaktivera varningar</translation>
     </message>
     <message>
         <location filename="../src/updateagent.cpp" line="68"/>
         <location filename="../src/updateagent.cpp" line="103"/>
         <source>Learn more</source>
-        <translation type="unfinished"></translation>
+        <translation>Läs mer</translation>
     </message>
     <message>
         <location filename="../src/updateagent.cpp" line="92"/>
         <source>Partial upgrade detected</source>
-        <translation type="unfinished"></translation>
+        <translation>Delvis uppgradering upptäckt</translation>
     </message>
     <message>
         <location filename="../src/updateagent.cpp" line="93"/>
         <source>You performed a &quot;partial upgrade&quot;. Please fully update your system to prevent system instability.
 Performing partial ugprades is unsupported.</source>
-        <translation type="unfinished"></translation>
+        <translation>Du har utfört en &quot;delvis-uppgradering&quot;. Uppdatera systemet helt för att förhindra systeminstabilitet.
+Det stöds inte att utföra delvisa uppgraderingar.</translation>
     </message>
     <message>
         <location filename="../src/updateagent.cpp" line="107"/>
         <source>Ignore permanently</source>
-        <translation type="unfinished"></translation>
+        <translation>Ignorera permanent</translation>
     </message>
 </context>
 </TS>
