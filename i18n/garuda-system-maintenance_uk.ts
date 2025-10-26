bd0df4caecc0b5b71de4430d791450498badb636
Garuda Linux CI/CD <gitlab@garudalinux.org>
2025-10-26 05:37:32 +0000

    chore(i18n): Pull Transifex translations

--- i18n/garuda-system-maintenance_uk.ts
+++ i18n/garuda-system-maintenance_uk.ts
@@ -1,17 +1,15 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="uk" sourcelanguage="en">
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="uk" sourcelanguage="en">
 <context>
     <name>ForumAgent</name>
     <message>
         <location filename="../src/forumagent.cpp" line="63"/>
         <source>New maintenance announcement: </source>
-        <translation type="unfinished"></translation>
+        <translation>Нове оголошення про технічне обслуговування:</translation>
     </message>
     <message>
         <location filename="../src/forumagent.cpp" line="65"/>
         <source>Open in browser</source>
-        <translation type="unfinished"></translation>
+        <translation>Відкрити у браузері</translation>
     </message>
 </context>
 <context>
@@ -19,45 +17,46 @@
     <message>
         <location filename="../src/packageagent.cpp" line="75"/>
         <source>Hotfix available!</source>
-        <translation type="unfinished"></translation>
+        <translation>Доступне виправлення!</translation>
     </message>
     <message>
         <location filename="../src/packageagent.cpp" line="75"/>
         <source>Important hotfix update found. Do you want to apply the hotfix (recommended)?
 Press the help button to learn more about this hotfix.</source>
-        <translation type="unfinished"></translation>
+        <translation>Знайдено важливе виправлення. Бажаєте застосувати виправлення (рекомендовано)?
+Натисніть кнопку довідки, щоб дізнатися більше про це виправлення.</translation>
     </message>
     <message>
         <location filename="../src/packageagent.cpp" line="88"/>
         <location filename="../src/packageagent.cpp" line="109"/>
         <location filename="../src/packageagent.cpp" line="111"/>
         <source>Garuda System Maintenance</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="../src/packageagent.cpp" line="88"/>
         <source>Updating keyrings in the background...</source>
-        <translation type="unfinished"></translation>
+        <translation>Оновлення ключів у фоновому режимі...</translation>
     </message>
     <message>
         <location filename="../src/packageagent.cpp" line="109"/>
         <source>Hotfix successfully applied!</source>
-        <translation type="unfinished"></translation>
+        <translation>Виправлення успішно застосовано!</translation>
     </message>
     <message>
         <location filename="../src/packageagent.cpp" line="109"/>
         <source>Keyrings successfully updated!</source>
-        <translation type="unfinished"></translation>
+        <translation>Ключі успішно оновлено!</translation>
     </message>
     <message>
         <location filename="../src/packageagent.cpp" line="111"/>
         <source>Hotfix failed!</source>
-        <translation type="unfinished"></translation>
+        <translation>Виправлення не вдалося!</translation>
     </message>
     <message>
         <location filename="../src/packageagent.cpp" line="111"/>
         <source>Keyring update failed!</source>
-        <translation type="unfinished"></translation>
+        <translation>Не вдалося оновити ключі!</translation>
     </message>
 </context>
 <context>
@@ -65,42 +64,42 @@ Press the help button to learn more about this hotfix.</source>
     <message>
         <location filename="../src/settingsdialog.ui" line="21"/>
         <source>Automatic updates</source>
-        <translation type="unfinished"></translation>
+        <translation>Автоматичні оновлення</translation>
     </message>
     <message>
         <location filename="../src/settingsdialog.ui" line="27"/>
         <source>Automatically update core keyrings</source>
-        <translation type="unfinished"></translation>
+        <translation>Автоматичне оновлення основних ключів</translation>
     </message>
     <message>
         <location filename="../src/settingsdialog.ui" line="52"/>
         <source>Prompt to apply critical hotfixes immediately</source>
-        <translation type="unfinished"></translation>
+        <translation>Запитувати про негайне застосування критичних виправлень</translation>
     </message>
     <message>
         <location filename="../src/settingsdialog.ui" line="64"/>
         <source>Notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Сповіщення</translation>
     </message>
     <message>
         <location filename="../src/settingsdialog.ui" line="70"/>
         <source>Notify about forum maintenance announcement posts</source>
-        <translation type="unfinished"></translation>
+        <translation>Повідомляти про технічне обслуговування форуму</translation>
     </message>
     <message>
         <location filename="../src/settingsdialog.ui" line="77"/>
         <source>Notify about partial upgrades</source>
-        <translation type="unfinished"></translation>
+        <translation>Повідомляти про часткові оновлення</translation>
     </message>
     <message>
         <location filename="../src/settingsdialog.ui" line="84"/>
         <source>Notify if system is critically out of date</source>
-        <translation type="unfinished"></translation>
+        <translation>Повідомляти, якщо система критично застаріла</translation>
     </message>
     <message>
         <location filename="../src/settingsdialog.ui" line="91"/>
         <source>Notify about old snapshots</source>
-        <translation type="unfinished"></translation>
+        <translation>Повідомляти про старі знімки/резервні копії</translation>
     </message>
 </context>
 <context>
@@ -108,44 +107,44 @@ Press the help button to learn more about this hotfix.</source>
     <message>
         <location filename="../src/snapshotagent.cpp" line="24"/>
         <source>Old snapshots/backups found</source>
-        <translation type="unfinished"></translation>
+        <translation>Знайдено старі знімки/резервні копії</translation>
     </message>
     <message>
         <location filename="../src/snapshotagent.cpp" line="25"/>
         <source>Old snapshots or snapshot restore backups have been found that are using up disk space.
 </source>
-        <translation type="unfinished"></translation>
+        <translation>Виявлено старі знімки або резервні копії відновлення знімків, які використовують місце на диску.</translation>
     </message>
     <message>
         <location filename="../src/snapshotagent.cpp" line="26"/>
         <source>View and delete</source>
-        <translation type="unfinished"></translation>
+        <translation>Переглянути та видалити</translation>
     </message>
     <message>
         <location filename="../src/snapshotagent.cpp" line="27"/>
         <source>Disable notifications</source>
-        <translation type="unfinished"></translation>
+        <translation>Вимкнути сповіщення</translation>
     </message>
     <message>
         <location filename="../src/snapshotagent.cpp" line="32"/>
         <source>Old snapshots</source>
-        <translation type="unfinished"></translation>
+        <translation>Старі знімки</translation>
     </message>
     <message>
         <location filename="../src/snapshotagent.cpp" line="36"/>
         <location filename="../src/snapshotagent.cpp" line="70"/>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>Видалити</translation>
     </message>
     <message>
         <location filename="../src/snapshotagent.cpp" line="66"/>
         <source>Old snapshots/backups found!</source>
-        <translation type="unfinished"></translation>
+        <translation>Знайдено старі знімки/резервні копії!</translation>
     </message>
     <message>
         <location filename="../src/snapshotagent.cpp" line="72"/>
         <source>Ignore permanently</source>
-        <translation type="unfinished"></translation>
+        <translation>Ігнорувати назавжди</translation>
     </message>
 </context>
 <context>
@@ -153,7 +152,7 @@ Press the help button to learn more about this hotfix.</source>
     <message>
         <location filename="../src/tray.ui" line="14"/>
         <source>Tray</source>
-        <translation type="unfinished"></translation>
+        <translation>Лоток</translation>
     </message>
 </context>
 <context>
@@ -162,49 +161,51 @@ Press the help button to learn more about this hotfix.</source>
         <location filename="../src/updateagent.cpp" line="25"/>
         <location filename="../src/updateagent.cpp" line="92"/>
         <source>System out of date</source>
-        <translation type="unfinished"></translation>
+        <translation>Система застаріла</translation>
     </message>
     <message>
         <location filename="../src/updateagent.cpp" line="26"/>
         <location filename="../src/updateagent.cpp" line="93"/>
         <source>This system has not been updated in a long time.
 Regularly applying system updates on a rolling release distribution is highly encouraged to avoid various issues.</source>
-        <translation type="unfinished"></translation>
+        <translation>Цю систему давно не оновлювали.
+Наполегливо рекомендується регулярно встановлювати оновлення системи на поточний випуск, щоб уникнути різних проблем.</translation>
     </message>
     <message>
         <location filename="../src/updateagent.cpp" line="27"/>
         <location filename="../src/updateagent.cpp" line="67"/>
         <location filename="../src/updateagent.cpp" line="98"/>
         <source>Update system</source>
-        <translation type="unfinished"></translation>
+        <translation>Оновлення системи</translation>
     </message>
     <message>
         <location filename="../src/updateagent.cpp" line="28"/>
         <location filename="../src/updateagent.cpp" line="69"/>
         <source>Disable warnings</source>
-        <translation type="unfinished"></translation>
+        <translation>Вимкнути попередження</translation>
     </message>
     <message>
         <location filename="../src/updateagent.cpp" line="68"/>
         <location filename="../src/updateagent.cpp" line="103"/>
         <source>Learn more</source>
-        <translation type="unfinished"></translation>
+        <translation>Дізнатися більше</translation>
     </message>
     <message>
         <location filename="../src/updateagent.cpp" line="92"/>
         <source>Partial upgrade detected</source>
-        <translation type="unfinished"></translation>
+        <translation>Виявлено часткове оновлення</translation>
     </message>
     <message>
         <location filename="../src/updateagent.cpp" line="93"/>
         <source>You performed a &quot;partial upgrade&quot;. Please fully update your system to prevent system instability.
 Performing partial upgrades is unsupported.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ви виконали «часткове оновлення». Будь ласка, повністю оновіть свою систему, щоб запобігти її нестабільності.
+Виконання часткових оновлень не підтримується.</translation>
     </message>
     <message>
         <location filename="../src/updateagent.cpp" line="107"/>
         <source>Ignore permanently</source>
-        <translation type="unfinished"></translation>
+        <translation>Ігнорувати назавжди</translation>
     </message>
 </context>
 </TS>
