cfd3e68f5c49ed320610154c9868ee37e10959d5
Transifex <group_7478400_bot_0b429cb480f35ded1257491259bd3e9f@noreply.gitlab.com>
2026-04-20 11:03:34 +0000

    Translate garuda-system-maintenance_en.ts in ka
    100% translated source file: 'garuda-system-maintenance_en.ts'
    on 'ka'.

---
+++ i18n/garuda-system-maintenance_ka.ts
@@ -0,0 +1,165 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="ka" sourcelanguage="en">
+<context>
+    <name>ForumAgent</name>
+    <message>
+        <location filename="../src/forumagent.cpp" line="63"/>
+        <source>New maintenance announcement: </source>
+        <translation>ახალი რემონტის გამოცხადება: </translation>
+    </message>
+    <message>
+        <location filename="../src/forumagent.cpp" line="65"/>
+        <source>Open in browser</source>
+        <translation>ბრაუზერში გახსნა</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../src/settingsdialog.ui" line="21"/>
+        <source>Automatic updates</source>
+        <translation>ავტომატური განახლებები</translation>
+    </message>
+    <message>
+        <location filename="../src/settingsdialog.ui" line="27"/>
+        <source>Automatically update core keyrings</source>
+        <translation>ბირთვის ბრელოკების ავტომატური განახლება</translation>
+    </message>
+    <message>
+        <location filename="../src/settingsdialog.ui" line="52"/>
+        <source>Prompt to apply critical hotfixes immediately</source>
+        <translation>კითხვა კრიტიკური პაჩების დაუყოვნებილი გადატარების შესახებ</translation>
+    </message>
+    <message>
+        <location filename="../src/settingsdialog.ui" line="64"/>
+        <source>Notifications</source>
+        <translation>გაფრთხილებები</translation>
+    </message>
+    <message>
+        <location filename="../src/settingsdialog.ui" line="70"/>
+        <source>Notify about forum maintenance announcement posts</source>
+        <translation>გაფრთხილება ფორუმის რემონტის გამოცხადების პოსტების შესახებ</translation>
+    </message>
+    <message>
+        <location filename="../src/settingsdialog.ui" line="77"/>
+        <source>Notify about partial upgrades</source>
+        <translation>გაფრთხილება ნაწილობრივი განახლებების შესახებ</translation>
+    </message>
+    <message>
+        <location filename="../src/settingsdialog.ui" line="84"/>
+        <source>Notify if system is critically out of date</source>
+        <translation>გაფრთხილება, თუ სისტემა კრიტიკულადაა მოძველებული</translation>
+    </message>
+    <message>
+        <location filename="../src/settingsdialog.ui" line="91"/>
+        <source>Notify about old snapshots</source>
+        <translation>გაფრთხილება ძველი სწრაფი ასლების შესახებ</translation>
+    </message>
+</context>
+<context>
+    <name>SnapshotAgent</name>
+    <message>
+        <location filename="../src/snapshotagent.cpp" line="24"/>
+        <source>Old snapshots/backups found</source>
+        <translation>აღმოჩენილია ძველი სწრაფი ასლები/მარქაფები</translation>
+    </message>
+    <message>
+        <location filename="../src/snapshotagent.cpp" line="25"/>
+        <source>Old snapshots or snapshot restore backups have been found that are using up disk space.
+</source>
+        <translation>აღმოჩენილია ძველი სწრაფი ასლები, ან სწრაფი ასლის აღდგენის მარქაფები, რომლებსაც დისკზე ადგილი მიაქვთ.
+</translation>
+    </message>
+    <message>
+        <location filename="../src/snapshotagent.cpp" line="26"/>
+        <source>View and delete</source>
+        <translation>ნახვა და წაშლა</translation>
+    </message>
+    <message>
+        <location filename="../src/snapshotagent.cpp" line="27"/>
+        <source>Disable notifications</source>
+        <translation>გაფრთხილებების გამორთვა</translation>
+    </message>
+    <message>
+        <location filename="../src/snapshotagent.cpp" line="32"/>
+        <source>Old snapshots</source>
+        <translation>ძველი სწრაფი ასლები</translation>
+    </message>
+    <message>
+        <location filename="../src/snapshotagent.cpp" line="36"/>
+        <location filename="../src/snapshotagent.cpp" line="70"/>
+        <source>Delete</source>
+        <translation>წაშლა</translation>
+    </message>
+    <message>
+        <location filename="../src/snapshotagent.cpp" line="66"/>
+        <source>Old snapshots/backups found!</source>
+        <translation>აღმოჩენილია ძველი სწრაფი ასლები/მარქაფები!</translation>
+    </message>
+    <message>
+        <location filename="../src/snapshotagent.cpp" line="72"/>
+        <source>Ignore permanently</source>
+        <translation>სამუდამოდ იგნორი</translation>
+    </message>
+</context>
+<context>
+    <name>Tray</name>
+    <message>
+        <location filename="../src/tray.ui" line="14"/>
+        <source>Tray</source>
+        <translation>თარო</translation>
+    </message>
+</context>
+<context>
+    <name>UpdateAgent</name>
+    <message>
+        <location filename="../src/updateagent.cpp" line="25"/>
+        <location filename="../src/updateagent.cpp" line="92"/>
+        <source>System out of date</source>
+        <translation>სისტემა მოძველებულია</translation>
+    </message>
+    <message>
+        <location filename="../src/updateagent.cpp" line="26"/>
+        <location filename="../src/updateagent.cpp" line="93"/>
+        <source>This system has not been updated in a long time.
+Regularly applying system updates on a rolling release distribution is highly encouraged to avoid various issues.</source>
+        <translation>სისტემა დიდი ახალია არ გაგიახლებიათ.
+სისტემური განახლებების რეგულარული გადატარება ამ ტიპის დისტრიბუტივზე მკაცრადაა რეკომენდებული სხვადასხვა პრობლემების თავიდან ასაცილებლად.</translation>
+    </message>
+    <message>
+        <location filename="../src/updateagent.cpp" line="27"/>
+        <location filename="../src/updateagent.cpp" line="67"/>
+        <location filename="../src/updateagent.cpp" line="98"/>
+        <source>Update system</source>
+        <translation>სისტემის განახლება</translation>
+    </message>
+    <message>
+        <location filename="../src/updateagent.cpp" line="28"/>
+        <location filename="../src/updateagent.cpp" line="69"/>
+        <source>Disable warnings</source>
+        <translation>გაფრთხილებების გამორთვა</translation>
+    </message>
+    <message>
+        <location filename="../src/updateagent.cpp" line="68"/>
+        <location filename="../src/updateagent.cpp" line="103"/>
+        <source>Learn more</source>
+        <translation>გაიგეთ მეტი</translation>
+    </message>
+    <message>
+        <location filename="../src/updateagent.cpp" line="92"/>
+        <source>Partial upgrade detected</source>
+        <translation>აღმოჩენილია ნაწილობრივი განახლება</translation>
+    </message>
+    <message>
+        <location filename="../src/updateagent.cpp" line="93"/>
+        <source>You performed a &quot;partial upgrade&quot;. Please fully update your system to prevent system instability.
+Performing partial upgrades is unsupported.</source>
+        <translation>თქვენ შეასრულეთ &apos;ნაწილობრივი განახლება&apos;. განაახლეთ სისტემა სრულად სისტემის არასტაბილურობის თავიდან ასაცილებლად.
+ნაწილობრივი განახლებების შესრულება მხარდაჭერილი არაა.</translation>
+    </message>
+    <message>
+        <location filename="../src/updateagent.cpp" line="107"/>
+        <source>Ignore permanently</source>
+        <translation>სამუდამოდ იგნორი</translation>
+    </message>
+</context>
+</TS>
